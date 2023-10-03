2cd3aef26fc106f72db3e7610958d254f98d3341
maxpipoka <martincardozo@gmail.com>
2023-10-03 12:25:11 -0300

    Avance

--- app/app.py
+++ app/app.py
@@ -1,27 +1,35 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+import os
+import psycopg2
+from dotenv import load_dotenv
 
-from db import get_db_connection
-from models import Student
+# Carga de las variables de entorno desde el .env
+load_dotenv()
 
-
-
+# Generación instancia aplicacion de Flask
 app = Flask(__name__)
 
-db = SQLAlchemy(app)
+# Guardado variable desde el entorno
+url_db = os.getenv('DB_URL')
 
+# Creacion instancia conexion
+connection = psycopg2.connect(url_db)
 
-@app.route('/')
+# Definicion endpoint del index
+@app.get('/')
 def index():
     return 'Hola Mundo'
 
-@app.route('/alumnos')
+# Definicion endpoint
+@app.get('/alumnos')
 def getAllAlumnos():
-    pass
+    return 'Todos los alumnos'
 
-@app.route('/alumnos/<id>')
+# Definicion endpoint
+@app.get('/alumnos/<id>')
 def getOneAlumno(id):
-    pass
+    return 'Un solo alumno'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
