d0e5675e8d828b5bf77dc2df242c74dec49161b5
maxpipoka <martincardozo@gmail.com>
2024-11-19 23:57:06 -0300

    Agregar modelo de usuario y endpoints de autenticación

---
+++ src/endpoints/auth.py
@@ -0,0 +1,37 @@
+from flask import Response, Blueprint, jsonify, request
+
+from ..models.models import User, db
+
+bp = Blueprint('auth', __name__)
+
+#Definicion endpoint realiza la autenticación de un usuario
+@bp.route('/auth', methods=['POST'])
+def loginUser():
+
+    try:
+        foundedUser = User.query.filter_by(User.username == request.json['username']).first()
+    except:
+        return Response({'message':'No se pudo obtener el usuario'}), 401
+    
+    if foundedUser:
+        if foundedUser.password == request.json['password']:
+            return Response({'message':'Usuario autenticado'}), 200
+        else:
+            return Response({'message':'Contraseña incorrecta'}), 401
+        
+#Definicion endpoint realiza el registro de un usuario
+@bp.route('/auth/register', methods=['POST'])
+def registerUser():
+    
+    try:
+        newUser = User(username=request.json['username'], 
+                       password=request.json['password'], 
+                       fullname=request.json['fullname'], 
+                       rol=request.json['rol'])
+        db.session.add(newUser)
+        db.session.commit()
+        
+    except Exception as e:
+        return jsonify({'message2': f'Error: {str(e)}'}), 400
+    
+    return Response({'message':'Usuario registrado'}), 200
