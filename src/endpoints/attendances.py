a2a6822c1574a68887b65e1e006e09afb9058d93
maxpipoka <martincardozo@gmail.com>
2025-01-26 10:23:05 -0300

    Corregir el orden de las asistencias por fecha en la función de obtención por ID de alumno

--- src/endpoints/attendances.py
+++ src/endpoints/attendances.py
@@ -79,7 +79,7 @@ def get_attendance_by_id(id):
 def get_attendances_by_student_id(id):
 
     try:
-        founded_attendances = Attendance.query.filter(Attendance.student_id == id).all().orderby(Attendance.day).asc()
+        founded_attendances = Attendance.query.filter(Attendance.student_id == id).order_by(Attendance.day.asc()).all()
 
     except:
         print('404 - No se pudo obtener las asistencias')
